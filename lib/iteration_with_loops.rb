ff1d79446e830578744285cb9d17abc2d76451f4
John Lovitsch <jmlovitsch@gmail.com>
2020-09-02 18:50:10 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,7 +9,7 @@ while row_index < src.count do
   element_index = 0
   
   while element_index < src[row_index].count do
-    if src[row_index][element_index] = src[row_index][element_index].to_s
+    if src[row_index][element_index] != src[row_index][element_index].to_s
       resulting_string << src[row_index][element_index]
     end
     element_index += 1
